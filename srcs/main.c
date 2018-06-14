13c4a5d100bfb5be2242bae0e33306d5da2d3058
mtan <findmarcg@gmail.com>
2018-06-14 16:54:21 -0700

    Added basic option parsing and error handle

--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,47 @@
 #include "ft_ls.h"
 
-int main()
+int		error(char *s, int err)
 {
-	ft_printf("hello ls\n");
+	if (err == USAGE)
+	{
+		ft_printf("ls: illegal option -- %c\n", *s);
+		ft_printf("usage: ls [-lRart] [file ...]\n");
+		exit(EXIT_FAILURE);
+	}
+	if (err == ERRNO)
+	{
+		ft_printf("ls: %s\n", strerror(errno));
+	}
+	return (0);
+}
+
+void	parse_option(char *s, int *flags)
+{
+	int		i;
+	while (*(++s))
+	{
+		if ((i = ft_strchri("lRart", *s)) == -1)
+			error(s, USAGE);
+		*flags |= (1 << i);
+	}
+}
+
+int		parse_options(int ac, char **av, int *flags)
+{
+	int		i;
+
+	*flags = 0;
+	i = 0;
+	while (++i < ac && av[i][0] == '-' && av[i][1])
+		parse_option(av[i], flags);
+	return (i);
+}
+
+int		main(int ac, char **av)
+{
+	int		i;
+	int		flags;
+
+	i = parse_options(ac, av, &flags);
 	return (0);
 }
