4c12bffbf28a7b62611fd2810a67807819a13845
mtan <findmarcg@gmail.com>
2018-06-14 16:53:20 -0700

    Added option definitions to header file

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -1,6 +1,23 @@
 #ifndef LS_H
 # define LS_H
 
+# include <stdlib.h>
+# include <errno.h>
+
 # include "libft.h"
+
+# define LS_L	1
+# define LS_RR	2
+# define LS_A	4
+# define LS_R	8
+# define LS_T	16
+
+enum { ERRNO, USAGE };
+
+// # define LS_D	32
+// # define LS_G	64
+// # define LS_ONE 128
+// # define LS_SS	256
+// # define LS_S	512
 
 #endif
